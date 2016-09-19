4825c6efd5a2e8547ef27ced96918e718c4ad6d1
James Regnier <jgreg071@gmail.com>
2016-09-19 10:25:37 -0500

    got price range select operational

--- app/controllers/WineSearchCtrl.js
+++ app/controllers/WineSearchCtrl.js
@@ -2,6 +2,8 @@
 
 app.controller('WineSearchCtrl', function ($scope, WineFactory, $location) {
 
+  $scope.priceRange = "";
+
   $scope.WineSearchParams = {
     wineName: "",
     numOfResults: "",
@@ -15,9 +17,44 @@ app.controller('WineSearchCtrl', function ($scope, WineFactory, $location) {
   };
 
   $scope.searchWine = () => {
+    setPrice();
     WineFactory.getWine($scope.WineSearchParams)
     .then ((wineList) => {
       $location.url('/wines/results');
     });
   };
+
+  var setPrice = () => {
+    var prices = $scope.priceRange.split("-");
+    console.log(prices);
+
+    $scope.WineSearchParams.minPrice = prices[0];
+    $scope.WineSearchParams.maxPrice = prices[1];
+
+    // if ($scope.priceRange === "0-25") {
+    //   $scope.WineSearchParams.maxPrice = 25;
+    //   $scope.WineSearchParams.minPrice = 0;
+    // }
+
+    // if ($scope.priceRange === "25-50") {
+    //   $scope.WineSearchParams.maxPrice = 50;
+    //   $scope.WineSearchParams.minPrice = 25;
+    // }
+
+    // if ($scope.priceRange === "50-75") {
+    //   $scope.WineSearchParams.maxPrice = 75;
+    //   $scope.WineSearchParams.minPrice = 50;
+    // }
+
+    // if ($scope.priceRange === "75-100") {
+    //   $scope.WineSearchParams.maxPrice = 100;
+    //   $scope.WineSearchParams.minPrice = 75;
+    // }
+
+    // if ($scope.priceRange === "+100") {
+    //   $scope.WineSearchParams.maxPrice = 1000;
+    //   $scope.WineSearchParams.minPrice = 100;
+    // }
+  };
+
 });
